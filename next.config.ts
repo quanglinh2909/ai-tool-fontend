c3d6275da178d5600e8e0897c7106b39d95d4302
quanglinh <19130118@st.hcmuaf.edu.vn>
2025-04-16 16:58:30 +0700

    init project

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    /* config options here */
+    reactStrictMode: true,
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+};
+
+
+export default nextConfig;
